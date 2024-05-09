ff25bae03685ca7ff949e7d6cd85468799bb666a
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-05-09 11:27:17 -0300

    feat: k8s deployment

--- app/src/app.controller.spec.ts
+++ app/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Olá, seja bem-vindo à nossa API NestJS com Prisma e MySQL');
     });
   });
 });
--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Olá, seja bem-vindo à nossa API NestJS com Prisma e MySQL';
   }
 }
