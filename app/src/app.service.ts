aee35a65bd897e9f6be01ecb2b856c5c0d68996a
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-05-16 09:11:57 -0300

    test5

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá, seja bem-vindo à nossa API NestJS com Prisma e MySQL - Versão 1.1';
+    return 'Olá, seja bem-vindo à nossa API NestJS com Prisma e MySQL - Versão 2';
   }
 }
